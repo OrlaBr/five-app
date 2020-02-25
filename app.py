cbc404896ec14a2c96bdf2c4f975ae12ca957ff7
OrlaBr <orlabreslin@yahoo.com>
2020-02-25 13:49:49 +0000

    Fixing edit and delete buttons positioning

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
 import os
-
 from os import path
 if path.exists("env.py"):
     import env
-
-
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
@@ -40,20 +37,21 @@ def insert_task():
 def edit_task(task_id):
     the_task = mongo.db.tasks.find_one({"_id": ObjectId(task_id)})
     all_categories = mongo.db.categories.find()
-    return render_template('edittask.html', task=the_task, categories=all_categories)
+    return render_template('edittask.html', task=the_task,
+                            categories=all_categories)
 
 
 @app.route('/update_task/<task_id>', methods=["POST"])
 def update_task(task_id):
     tasks = mongo.db.tasks
     tasks.update({'_id': ObjectId(task_id)},
-    {
-        'task_name': request.form.get('task_name'),
-        'category_name': request.form.get('category_name'),
-        'task_description': request.form.get('task_description'),
-        'due_date': request.form.get('due_date'),
-        'is_urgent': request.form.get('is_urgent')
-    })
+        {
+            'task_name': request.form.get('task_name'),
+            'category_name': request.form.get('category_name'),
+            'task_description': request.form.get('task_description'),
+            'due_date': request.form.get('due_date'),
+            'is_urgent': request.form.get('is_urgent')
+        })
     return redirect(url_for('get_tasks'))
 
 
@@ -61,6 +59,12 @@ def update_task(task_id):
 def delete_task(task_id):
     mongo.db.tasks.remove({'_id': ObjectId(task_id)})
     return redirect(url_for('get_tasks'))
+
+
+@app.route('/get_categories')
+def get_categories():
+    return render_template('categories.html',
+                        categories=mongo.db.categories.find())
 
 
 if __name__ == '__main__':
