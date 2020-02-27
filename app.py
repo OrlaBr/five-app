e41dda9424f9f3b79e655bdde230d2da5255fc7b
OrlaBr <orlabreslin@yahoo.com>
2020-02-27 13:43:42 +0000

    Create About page

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 import os
+# !hiding password
 from os import path
 if path.exists("env.py"):
     import env
-
+# import tools
 from flask import Flask, render_template, redirect, request, url_for, session, flash
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
 app = Flask(__name__)
-
+# creating link to database
 app.config["MONGO_DBNAME"] = 'five-app'
 app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
 
@@ -27,29 +28,35 @@ def get_login():
 
 
 # register page
-@app.route('/get_register')
+@app.route('/get_register', methods=['GET', 'POST'])
 def get_register():
     return render_template("register.html")
 
 
+# about page
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+# tasks page
 @app.route('/get_tasks')
 def get_tasks():
     return render_template("tasks.html", tasks=mongo.db.tasks.find())
 
-
+# add tasks page
 @app.route('/add_task')
 def add_task():
     return render_template('addtask.html',
                            categories=mongo.db.categories.find())
 
-
+# adding task to mongodb
 @app.route('/insert_task', methods=['POST'])
 def insert_task():
     tasks = mongo.db.tasks
     tasks.insert_one(request.form.to_dict())
     return redirect(url_for('get_tasks'))
 
-
+# editing tasks on database
 @app.route('/edit_task/<task_id>')
 def edit_task(task_id):
     the_task = mongo.db.tasks.find_one({"_id": ObjectId(task_id)})
@@ -57,7 +64,7 @@ def edit_task(task_id):
     return render_template('edittask.html', task=the_task,
                             categories=all_categories)
 
-
+# updating tasks on database
 @app.route('/update_task/<task_id>', methods=["POST"])
 def update_task(task_id):
     tasks = mongo.db.tasks
@@ -71,26 +78,26 @@ def update_task(task_id):
         })
     return redirect(url_for('get_tasks'))
 
-
+# deleting tasks from database
 @app.route('/delete_task/<task_id>')
 def delete_task(task_id):
     mongo.db.tasks.remove({'_id': ObjectId(task_id)})
     return redirect(url_for('get_tasks'))
 
-
+# getting categories from database
 @app.route('/get_categories')
 def get_categories():
     return render_template('categories.html',
                             categories=mongo.db.categories.find())
 
-
+# editing categories on database
 @app.route('/edit_category/<category_id>')
 def edit_category(category_id):
     return render_template('editcategory.html',
                             category=mongo.db.categories.find_one(
                                 {'_id': ObjectId(category_id)}))
 
-
+# updating categories in database
 @app.route('/update_category/<category_id>', methods=['POST'])
 def update_category(category_id):
     mongo.db.categories.update(
@@ -98,20 +105,20 @@ def update_category(category_id):
         {'category_name': request.form.get('category_name')})
     return redirect(url_for('get_categories'))
 
-
+# deleting categories on database
 @app.route('/delete_category/<category_id>')
 def delete_category(category_id):
     mongo.db.categories.remove({'_id': ObjectId(category_id)})
     return redirect(url_for('get_categories'))
 
-
+# adding categories on database
 @app.route('/insert_category', methods=['POST'])
 def insert_category():
     category_doc = {'category_name': request.form.get('category_name')}
     mongo.db.categories.insert_one(category_doc)
     return redirect(url_for('get_categories'))
 
-
+# add category template
 @app.route('/add_category')
 def add_category():
     return render_template('addcategory.html')
