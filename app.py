914fd8ac334f033ed305607c0771556429e1e989
OrlaBr <orlabreslin@yahoo.com>
2020-03-06 12:03:11 +0000

    404 not working - remove files

--- app.py
+++ app.py
@@ -124,11 +124,6 @@ def add_category():
     return render_template('addcategory.html')
 
 
-@app.errorhandler(404)
-def not_found(e):
-    return render_template("404.html")
-
-
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
